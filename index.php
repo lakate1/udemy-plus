21312482abcb55b1e9fbaadda5a1ac469524220f
Leah Queen <leah836@gmail.com>
2024-01-24 16:46:53 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Plugin Name:       Udemy Plus
+ * Plugin URI:        https://udemy.com
+ * Description:       A plugin for adding blocks to the theme.
+ * Version:           1.0.0
+ * Requires at least: 6.4.2
+ * Requires PHP:      7.2
+ * Author:            Queen Codes
+ * Author URI:        https://author.example.com/
+* Text Domain:       udemy-plus
+ * Domain Path:       /languages
+ */
+
+if(!function_exists('add_action')) {
+    echo 'Seems like you stumbled here by accident';
+}
